26a065e84a95b971781b6de4487d3d0df44af36b
Muhammad Zeeshan Abbas <zzabbaskhan830@gmail.com>
2025-08-10 21:29:22 +0500

    Update AxiosInstance.jsx

--- src/Store/AxiosInstance.jsx
+++ src/Store/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000', // set your base URL here
+  baseURL: 'https://ecommerce-zeeshan-135137aebf89.herokuapp.com/', // set your base URL here
   withCredentials: true, // send cookies (optional)
 });
 
